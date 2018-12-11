f1df976820d34cf3d4691f6c520bb6500f611d09
tinbie <t.biehle@yahoo.de>
2018-12-11 21:56:02 +0100

    Shown capture is now in RGB.

--- face_detection_cap.py
+++ face_detection_cap.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
         faces = face_detect.detectMultiScale(gray_img, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30))
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(gray_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-        cv2.imshow('face-detection', gray_img)
+        cv2.imshow('face-detection', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
