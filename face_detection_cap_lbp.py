8ed8e1a81c1dbd96d5fd59c2f0635526b469b037
tinbie <t.biehle@yahoo.de>
2018-12-16 11:45:43 +0100

    Add a LBP-classifier which is about 3 times faster.

---
+++ face_detection_cap_lbp.py
@@ -0,0 +1,26 @@
+#python2.7 utf8
+
+import numpy
+import cv2
+from time import sleep
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+    sleep(1)
+
+    while True:
+        frame = cap.read()[1]
+        gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face_detect = cv2.CascadeClassifier('lbp_facedetection.xml')
+        faces = face_detect.detectMultiScale(gray_img, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30))
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        cv2.imshow('face-detection', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows
